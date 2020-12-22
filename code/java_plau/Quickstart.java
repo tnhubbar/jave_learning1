91d7b179497531a201bfe4cf69924dd1ab846a2b
tnhubbar <taryn.johnson919@gmail.com>
2020-12-22 18:33:03 -0500

    testing

---
+++ code/java_plau/Quickstart.java
@@ -0,0 +1,10 @@
+public class Quickstart {
+    public static void main (String[] args) {
+        System.out.println("Hello There World!");
+
+        byte myMinByteValue = Byte.MIN_VALUE; 
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue );
+        
+    }
+}
