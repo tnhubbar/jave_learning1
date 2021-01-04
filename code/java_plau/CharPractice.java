5174eb4bd5e3518c9785a7727ed9ad5269996a5b
tnhubbar <taryn.johnson919@gmail.com>
2021-01-03 19:26:27 -0500

    Char Java Pracetice

---
+++ code/java_plau/CharPractice.java
@@ -0,0 +1,13 @@
+
+public class CharPractice {
+
+    public static void main (String[] args ){
+
+        //write your code here 
+
+        char myChar = 'T';
+
+        System.out.println("My char is " + myChar);
+    }
+
+}
