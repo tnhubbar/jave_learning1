a239504d7394098a45094eaba6845d6a5bf4e211
tnhubbar <taryn.johnson919@gmail.com>
2021-01-10 21:42:12 -0500

    String Practice learning Java

---
+++ code/java_plau/StringPractice.java
@@ -0,0 +1,10 @@
+public class StringPractice {
+    public static void main (String[] args) {
+        System.out.println("Hello There World!");
+        // String is a datatype in Java, and not a primitive type. It's actually a class. 
+      
+       
+
+        
+    }
+}
